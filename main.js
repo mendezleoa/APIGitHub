2b8f7afa4af1f6b463ffeadb5bcdb245fa9a3490
mendezleoa <92312161+mendezleoa@users.noreply.github.com>
2023-07-08 12:12:34 -0400

    Agregados timer y eventos

--- main.js
+++ main.js
@@ -1,17 +1,46 @@
 const resultados = document.getElementById("resultados")
 
-let user = "ferxxo"
+const form = document.getElementById("form")
+let timer;
 
-fetch('https://api.github.com/search/users?q='+ user )
-.then((resp) => resp.json())
-.then((data) => {
-  console.log(data.items);
-  fetch(data.items[0].url)
-  .then((resp_u) => resp_u.json())
-  .then((data_u) => {
-    console.log(data_u);
-  })
+form.addEventListener("input", () => {
+
+  clearTimeout(timer);
+
+  timer = setTimeout(() => {
+    let users = document.getElementById("text-input").value
+    fetch('https://api.github.com/search/users?q='+ users )
+    .then((resp) => resp.json())
+    .then((data) => {
+      console.log(data.items);
+      resultados.innerHTML = '';
+      data.items.slice(0,5).forEach(user => {
+        buscarUsuario(user.url);
+      });
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  }, 700)
 })
-.catch((error) => {
-  console.log(error)
-})
+
+function buscarUsuario(user) {
+  fetch(user)
+    .then((resp_u) => resp_u.json())
+    .then((data_u) => {
+      console.log(data_u);
+      let bio = '';
+      if (!data_u.bio) {
+        bio = "Sin biografía"
+      } else {
+        bio = data_u.bio
+      }
+      resultados.innerHTML += `
+      <div class="tarjeta-usuario">
+        <img src="${data_u.avatar_url}">
+        <h2>Id: ${data_u.login}</h2>
+        <p>Repositorios: ${data_u.public_repos}</p>
+        <p>Biografía: ${bio}</p>
+      </div>`;
+    })
+}
