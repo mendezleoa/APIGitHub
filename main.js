a29a1f66e452021be2cb072a6dcc420af670d67c
mendezleoa <92312161+mendezleoa@users.noreply.github.com>
2023-07-08 13:54:18 -0400

    Agregados estilos con temas

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const resultados = document.getElementById("resultados")
 
-const form = document.getElementById("form")
+const botonTemas = document.getElementById("temas");
+const form = document.getElementById("form");
 let timer;
 
 form.addEventListener("input", () => {
@@ -19,7 +20,7 @@ form.addEventListener("input", () => {
       });
     })
     .catch((error) => {
-      console.log(error)
+      console.log(error);
     })
   }, 700)
 })
@@ -31,16 +32,22 @@ function buscarUsuario(user) {
       console.log(data_u);
       let bio = '';
       if (!data_u.bio) {
-        bio = "Sin biografía"
+        bio = "Sin biografía";
       } else {
-        bio = data_u.bio
+        bio = data_u.bio;
       }
       resultados.innerHTML += `
-      <div class="tarjeta-usuario">
-        <img src="${data_u.avatar_url}">
-        <h2>Id: ${data_u.login}</h2>
-        <p>Repositorios: ${data_u.public_repos}</p>
-        <p>Biografía: ${bio}</p>
+      <div class="tarjeta">
+        <img src="${data_u.avatar_url}" class="avatar">
+        <div class="datos">
+          <h2>Id: ${data_u.login}</h2>
+          <p>Repositorios: ${data_u.public_repos}</p>
+          <p class="bio">Biografía: ${bio}</p>
+        </div>
       </div>`;
     })
 }
+
+botonTemas.addEventListener("click", () => {
+  document.body.classList.toggle("dark")
+});
